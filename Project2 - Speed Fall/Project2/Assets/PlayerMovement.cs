707313056b6090c137a426535ada078766f74246
Ricky Tham <rickytham123@gmail.com>
2018-02-16 01:26:00 -0800

    Added some scripts

---
+++ Project2 - Speed Fall/Project2/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] Rigidbody2D rgbd;
+    [SerializeField] Animator anim;
+    bool isMoving, isJumping;
+
+    private void FixedUpdate()
+    {
+        isMoving = false;
+        isJumping = false;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+        }
+    }
+
+    void Move(Direction d)
+    {
+        rgbd.MovePosition((Vector2)gameObject.transform.position + (int)d * Vector2.right)
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Project2 - Speed Fall/Project2/Assets/scripts/ScrollingBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    [SerializeField] GameObject camera;
+    [SerializeField] Sprite background;
+    [SerializeField] GameObject[] backgrounds;
+    int currentBackground = 0;
+    float startPos = 0;
+
+	// Use this for initialization
+	void Start () {
+	    backgrounds[1].transform.position = new Vector3(backgrounds[1].transform.position.x, background.bounds.min.y * 2, backgrounds[1].transform.position.z);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (camera.transform.position.y <= startPos + background.bounds.min.y * 2)
+        {
+            if (currentBackground == 1)
+            {
+                backgrounds[1].transform.position -= Vector3.down * background.bounds.min.y * 4;
+                currentBackground = 0;
+            }
+            else
+            {
+                backgrounds[0].transform.position -= Vector3.down * background.bounds.min.y * 4;
+                currentBackground = 1;
+            }
+            startPos = camera.transform.position.y;
+        }
+	}
+}
