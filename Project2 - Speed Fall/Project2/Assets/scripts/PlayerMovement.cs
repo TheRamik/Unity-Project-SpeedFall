38f2bb3fd6f5e22efa7c52989dcad5c61c1eacb1
Ricky Tham <rickytham123@gmail.com>
2018-02-19 04:31:51 -0800

    New Update

--- Project2 - Speed Fall/Project2/Assets/scripts/PlayerMovement.cs
+++ Project2 - Speed Fall/Project2/Assets/scripts/PlayerMovement.cs
@@ -4,60 +4,70 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public bool isGrounded, isWall;
+    public bool isGrounded, isWall, facingRight;
     public GameObject starPrefab;
     public GameObject starSpawner;
     private Rigidbody2D rgbd;
-    bool isMoving, isJumping, isWallSliding, facingRight;
+    private Animator anim;
+    public Transform wallPoint;
+    public LayerMask wallLayerMask;
+    bool isMoving, isJumping, isWallSliding;
 
     [SerializeField] private float jumpForce = 300f;
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] Animator anim;
     [SerializeField] private LayerMask whatIsGround;
-    [SerializeField]private float groundRadius;
+    [SerializeField] private float groundRadius;
     [SerializeField] private Transform[] groundPoints;
-    public Transform wallPoint;
-    public LayerMask wallLayerMask;
+
 
     private void Awake()
     {
         rgbd = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         facingRight = true;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        isWall = false;
         isMoving = false;
         isJumping = false;
         isGrounded = IsGrounded();
+        anim.SetBool("isGrounded", isGrounded);
 
         HandleInput();
         HandleMovement(horizontal);
+        if (rgbd.velocity.y < -0.5f)
+        {
+            anim.SetBool("land", true);
+        }
+        HandleWallSliding(horizontal);
+        HandleLayers();
     }
 
     private void HandleInput()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && !isWallSliding)
         {
-            isJumping = !isJumping;
+            isJumping = true;
             HandleJump();
         }
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.rotation = new Quaternion(0, 180, 0, 0);
-            facingRight = !facingRight;
+            facingRight = false;
             isMoving = !isMoving;
         }
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
-            facingRight = !facingRight;
+            facingRight = true;
             isMoving = !isMoving;
         }
         if (Input.GetKey(KeyCode.K))
         {
-            ShootingStar();
+            ShootingStar(0);
         }
 
     }
@@ -68,6 +78,19 @@ public class PlayerMovement : MonoBehaviour {
         {
             isJumping = false;
             rgbd.AddForce(new Vector2(0f, jumpForce));
+            anim.SetTrigger("jump");
+        }
+    }
+
+    private void HandleLayers()
+    {
+        if(!isGrounded)
+        {
+            anim.SetLayerWeight(1, 1);
+        }
+        else
+        {
+            anim.SetLayerWeight(1, 0);
         }
     }
 
@@ -76,20 +99,23 @@ public class PlayerMovement : MonoBehaviour {
         if (!isGrounded)
         {
             isWall = Physics2D.OverlapCircle(wallPoint.position, 0.1f, wallLayerMask);
-            if ((facingRight && (horizontal > 0.1f)) || (!facingRight && (horizontal < 0.1f)))
+            
+            if ((facingRight && (horizontal >= 0f)) || (!facingRight && (horizontal < 0.1f)))
             {
-                if (isWall)
-                {
-                    rgbd.velocity = new Vector2(rgbd.velocity.x, -0.7f);
-                    isWallSliding = true;
-                }
+                WallSliding();
             }
+        }
+        if (isWall = false || isGrounded)
+        {
+            isWallSliding = false;
         }
     }
 
     private void HandleMovement(float horizontal)
     {
+
         rgbd.velocity = new Vector2(horizontal * moveSpeed, rgbd.velocity.y);
+        anim.SetFloat("speed", Mathf.Abs(horizontal));
     }
 
     private bool IsGrounded()
@@ -103,6 +129,9 @@ public class PlayerMovement : MonoBehaviour {
                 {
                     if (colliders[i].gameObject != gameObject)
                     {
+                        anim.ResetTrigger("jump");
+                        anim.SetBool("land", false);
+                        anim.SetBool("isWallSliding", false);
                         return true;
                     }
                 }
@@ -111,20 +140,48 @@ public class PlayerMovement : MonoBehaviour {
         return false;
     }
 
-    void ShootingStar()
+    private void WallSliding()
     {
-        if (facingRight)
+        if (isWall)
         {
-            GameObject star = (GameObject)Instantiate(starPrefab, transform.position, Quaternion.identity);
-            star.transform.position = starSpawner.transform.position;
-            star.GetComponent<NinjaStar>().Initialize(Vector2.right);
-            
+            rgbd.velocity = new Vector2(0, -0.9f);
+            isWallSliding = true;
+            anim.ResetTrigger("jump");
+            anim.SetBool("land", false);
+            anim.SetBool("isWallSliding", true);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (facingRight)
+                {
+                    rgbd.AddForce(new Vector2(-jumpForce + 200f, jumpForce));
+                }
+                else
+                {
+                    rgbd.AddForce(new Vector2(jumpForce + 200f, jumpForce));
+                }
+            }
         }
-        else
+    }
+
+    public void ShootingStar(int value)
+    {
+        if (!isGrounded && value == 1 || isGrounded && value == 0)
         {
-            GameObject star = (GameObject)Instantiate(starPrefab, transform.position, Quaternion.identity);
-            star.transform.position = starSpawner.transform.position;
-            star.GetComponent<NinjaStar>().Initialize(Vector2.left);
+            if (facingRight)
+            {
+                GameObject star = (GameObject)Instantiate(starPrefab, transform.position, Quaternion.identity);
+                star.transform.position = starSpawner.transform.position;
+                star.GetComponent<NinjaStar>().Initialize(Vector2.right);
+
+            }
+            else
+            {
+                GameObject star = (GameObject)Instantiate(starPrefab, transform.position, Quaternion.identity);
+                star.transform.position = starSpawner.transform.position;
+                star.GetComponent<NinjaStar>().Initialize(Vector2.left);
+            }
         }
+
+
     }
 }
